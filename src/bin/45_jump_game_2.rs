f173d8dd673b9d2968e55413d72ddb10d2dbe014
lilhammer111 <wwwwwdemon@gmail.com>
2024-09-05 17:51:44 +0800

    wip: 45

--- src/bin/45_jump_game_2.rs
+++ src/bin/45_jump_game_2.rs
@@ -1,5 +1,7 @@
+use std::cmp::min;
+
 fn main() {
-    let nums = vec![2, 3, 1, 1, 4];
+    let nums = vec![10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 0];
     let result = Solution::jump(nums);
     println!("result: {}", result);
 }
@@ -7,6 +9,27 @@ fn main() {
 struct Solution;
 impl Solution {
     pub fn jump(nums: Vec<i32>) -> i32 {
-        0
+        let mut i = 0;
+        let n = nums.len();
+        let mut count = 0;
+        let mut max_val = 0;
+        while i != n - 1 {
+            count += 1;
+            let j = nums[i];
+            i += 1;
+            if i + j as usize >= n {
+                return count;
+            }
+            let max_distance = min(i + j as usize, n);
+            for idx in i..max_distance {
+                println!("ele: {}", nums[idx]);
+                if nums[idx] >= max_val {
+                    i = idx;
+                    max_val = nums[idx]
+                }
+            }
+            max_val = 0;
+        }
+        count
     }
 }
